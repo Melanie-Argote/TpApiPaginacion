4dbf4200c71675e52c7b15edf35516a74cb89828
Melanie <melanie.argote17@gmail.com>
2023-10-11 22:16:57 -0300

    Primer commit!

---
+++ src/main/java/com/example/demo/services/DomiclioServiceImpl.java
@@ -0,0 +1,18 @@
+package com.example.demo.services;
+
+import com.example.demo.entities.Domicilio;
+import com.example.demo.repositories.BaseRepository;
+import com.example.demo.repositories.DomicilioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DomiclioServiceImpl extends BaseServiceImpl<Domicilio, Long> implements DomicilioService{
+    @Autowired
+    private DomicilioRepository domicilioRepository;
+
+    public DomiclioServiceImpl(BaseRepository<Domicilio, Long> baseRepository, DomicilioRepository domicilioRepository) {
+        super(baseRepository);
+        this.domicilioRepository = domicilioRepository;
+    }
+}
